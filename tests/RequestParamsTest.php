f2fc8eae546fcd0c55c8f062ceb0d5926c4d5aaf
Veronica Lupo Pasini <v.lupopasini@esconsulting.it>
2020-03-05 16:14:02 +0100

    Added tests

--- tests/RequestParamsTest.php
+++ tests/RequestParamsTest.php
@@ -4,21 +4,59 @@ namespace Esc\Tests;
 
 use Esc\RequestParams;
 use PHPUnit\Framework\TestCase;
+use Prophecy\Argument;
+use Prophecy\Prophet;
 use Symfony\Component\HttpFoundation\Request;
 
-class RequestParamsTest extends TestCase
+class RequestParamsProphecyTest extends TestCase
 {
-    public function testRequestParamsFiltersReturnException(): void
-    {
-        $request = $this->getMockBuilder(Request::class)
-            ->getMock();
-        $request->expects($this->at(0))
-            ->method('get')
-            ->with($this->equalTo('filters'), $this->equalTo(''))
-            ->willReturn(null);
+    private $request;
 
-        $this->expectException(\Exception::class);
-        RequestParams::fromRequest($request);
+    public function setUp(): void
+    {
+        parent::setUp();
+        $prophet = new Prophet();
+        $this->request = $prophet->prophesize(Request::class);
+    }
+
+    public function stringProvider(): array
+    {
+        return [
+            [''],
+            ['foo'],
+            ['hello world']
+        ];
+    }
+
+    /**
+     * @dataProvider stringProvider
+     * @param $returnValue
+     */
+    public function testRequestParamsFiltersReturnJsonException($returnValue): void
+    {
+        $this->request->get('filters', '{}')->willReturn($returnValue);
+        $this->expectException(\JsonException::class);
+        RequestParams::fromRequest($this->request->reveal());
+    }
+
+    public function filtersProvider(): array
+    {
+        return [
+            [null],
+            [false],
+            [[]]
+        ];
+    }
+
+    /**
+     * @dataProvider filtersProvider
+     * @param $returnValue
+     */
+    public function testRequestParamsFiltersReturnException($returnValue): void
+    {
+        $this->request->get('filters', '{}')->willReturn($returnValue);
+        $this->expectException(\RuntimeException::class);
+        RequestParams::fromRequest($this->request->reveal());
     }
 
     public function testRequestParamsFiltersResultBasedOnRequestFilters(): void
@@ -26,223 +64,85 @@ class RequestParamsTest extends TestCase
         $expectedFilters = [
             'foo' => 'bar'
         ];
-        $request = $this->getMockBuilder(Request::class)
-            ->getMock();
-        $request->expects($this->at(0))
-            ->method('get')
-            ->with($this->equalTo('filters'), $this->equalTo(''))
-            ->willReturn('{"foo": "bar"}');
-
-        $actualRequestParams = RequestParams::fromRequest($request);
+        $this->request->get('filters', '{}')->willReturn('{"foo": "bar"}');
+        $actualRequestParams = RequestParams::fromRequest($this->request->reveal());
         $actualFilters = $actualRequestParams->get('filters');
         $this->assertEquals($expectedFilters, $actualFilters);
     }
 
     public function testIfRequestParamsSetEmptyArrayWhenSortByIsNull(): void
     {
-        $expectedValue = [];
-        $request = $this->getMockBuilder(Request::class)
-            ->getMock();
-
-        $request->expects($this->at(0))
-            ->method('get')
-            ->with($this->equalTo('filters'), $this->equalTo(''))
-            ->willReturn('{"foo": "bar"}');
-
-        $request->expects($this->at(1))
-            ->method('get')
-            ->with($this->equalTo('sortBy'))
-            ->willReturn(null);
-
-        $requestParams = RequestParams::fromRequest($request);
+        $this->request->get('filters', '{}')->willReturn('{"foo": "bar"}');
+        $this->request->get('sortBy')->willReturn(null);
+        $requestParams = RequestParams::fromRequest($this->request->reveal());
         $requestParamsValue = $requestParams->get('sortBy');
-        $this->assertEquals($expectedValue, $requestParamsValue);
+        $this->assertEquals([], $requestParamsValue);
     }
 
     public function testIfDescendingIsTrue(): void
     {
-        $expectedValue =[
+        $expectedValue = [
             'foo' => 'DESC',
         ];
-
-        $request = $this->getMockBuilder(Request::class)
-            ->getMock();
-
-        $request->expects($this->at(0))
-            ->method('get')
-            ->with($this->equalTo('filters'), $this->equalTo(''))
-            ->willReturn('{"foo": "bar"}');
-
-        $request->expects($this->at(1))
-            ->method('get')
-            ->with($this->equalTo('sortBy'))
-            ->willReturn('foo');
-
-        $request->expects($this->at(2))
-            ->method('get')
-            ->with($this->equalTo('descending'))
-            ->willReturn(true);
-
-        $requestParams = RequestParams::fromRequest($request);
+        $this->request->get('filters', '{}')->willReturn('{"foo": "bar"}');
+        $this->request->get('sortBy')->willReturn('foo');
+        $this->request->get('descending')->willReturn(true);
+        $requestParams = RequestParams::fromRequest($this->request->reveal());
         $requestParamsValue = $requestParams->get('sortBy');
         $this->assertEquals($expectedValue, $requestParamsValue);
     }
 
     public function testIfDescendingIsFalse(): void
     {
-        $expectedValue =[
+        $expectedValue = [
             'foo' => 'ASC',
         ];
-
-        $request = $this->getMockBuilder(Request::class)
-            ->getMock();
-
-        $request->expects($this->at(0))
-            ->method('get')
-            ->with($this->equalTo('filters'), $this->equalTo(''))
-            ->willReturn('{"foo": "bar"}');
-
-        $request->expects($this->at(1))
-            ->method('get')
-            ->with($this->equalTo('sortBy'))
-            ->willReturn('foo');
-
-        $request->expects($this->at(2))
-            ->method('get')
-            ->with($this->equalTo('descending'))
-            ->willReturn(false);
-
-        $requestParams = RequestParams::fromRequest($request);
+        $this->request->get('filters', '{}')->willReturn('{"foo": "bar"}');
+        $this->request->get('sortBy')->willReturn('foo');
+        $this->request->get('descending')->willReturn(false);
+        $requestParams = RequestParams::fromRequest($this->request->reveal());
         $requestParamsValue = $requestParams->get('sortBy');
         $this->assertEquals($expectedValue, $requestParamsValue);
     }
 
     public function testRowsPerPageAreEqualToLimitWhenRowsPerPageAreMoreThan0(): void
     {
-        $request = $this->getMockBuilder(Request::class)
-            ->getMock();
-
-        $request->expects($this->at(0))
-            ->method('get')
-            ->with($this->equalTo('filters'), $this->equalTo(''))
-            ->willReturn('{"foo": "bar"}');
-
-        $request->expects($this->at(1))
-            ->method('get')
-            ->with($this->equalTo('sortBy'))
-            ->willReturn('foo');
-
-        $request->expects($this->at(2))
-            ->method('get')
-            ->with($this->equalTo('descending'))
-            ->willReturn(false);
-
-        $request->expects($this->at(3))
-            ->method('get')
-            ->with($this->equalTo('rowsPerPage'))
-            ->willReturn(3);
-
-        $requestParams = RequestParams::fromRequest($request);
+        $this->request->get('filters', '{}')->willReturn('{"foo": "bar"}');
+        $this->request->get(Argument::cetera())->willReturn(null);
+        $this->request->get('rowsPerPage')->willReturn(3);
+        $requestParams = RequestParams::fromRequest($this->request->reveal());
         $requestParamsValue = $requestParams->get('limit');
         $this->assertEquals(3, $requestParamsValue);
     }
 
     public function testLimitIsNullWhenRowsPerPageAre0(): void
     {
-        $request = $this->getMockBuilder(Request::class)
-            ->getMock();
-
-        $request->expects($this->at(0))
-            ->method('get')
-            ->with($this->equalTo('filters'), $this->equalTo(''))
-            ->willReturn('{"foo": "bar"}');
-
-        $request->expects($this->at(1))
-            ->method('get')
-            ->with($this->equalTo('sortBy'))
-            ->willReturn('foo');
-
-        $request->expects($this->at(2))
-            ->method('get')
-            ->with($this->equalTo('descending'))
-            ->willReturn(false);
-
-        $request->expects($this->at(3))
-            ->method('get')
-            ->with($this->equalTo('rowsPerPage'))
-            ->willReturn(0);
-
-        $requestParams = RequestParams::fromRequest($request);
+        $this->request->get('filters', '{}')->willReturn('{"foo": "bar"}');
+        $this->request->get(Argument::cetera())->willReturn(null);
+        $this->request->get('rowsPerPage')->willReturn(0);
+        $requestParams = RequestParams::fromRequest($this->request->reveal());
         $requestParamsValue = $requestParams->get('limit');
         $this->assertEquals(null, $requestParamsValue);
     }
 
-    public function testIfOffsetIsNot0WhenPageAreMoreThan0(): void
+    public function testIfOffsetIsNot0(): void
     {
-        $request = $this->getMockBuilder(Request::class)
-            ->getMock();
-
-        $request->expects($this->at(0))
-            ->method('get')
-            ->with($this->equalTo('filters'), $this->equalTo(''))
-            ->willReturn('{"foo": "bar"}');
-
-        $request->expects($this->at(1))
-            ->method('get')
-            ->with($this->equalTo('sortBy'))
-            ->willReturn('foo');
-
-        $request->expects($this->at(2))
-            ->method('get')
-            ->with($this->equalTo('descending'))
-            ->willReturn(false);
-
-        $request->expects($this->at(3))
-            ->method('get')
-            ->with($this->equalTo('rowsPerPage'))
-            ->willReturn(10);
-
-        $request->expects($this->at(4))
-            ->method('get')
-            ->with($this->equalTo('page'))
-            ->willReturn(2);
-
-        $requestParams = RequestParams::fromRequest($request);
+        $this->request->get('filters', '{}')->willReturn('{"foo": "bar"}');
+        $this->request->get(Argument::cetera())->willReturn(null);
+        $this->request->get('rowsPerPage')->willReturn(10);
+        $this->request->get('page')->willReturn(2);
+        $requestParams = RequestParams::fromRequest($this->request->reveal());
         $requestParamsValue = $requestParams->get('offset');
         $this->assertEquals(10, $requestParamsValue);
     }
 
     public function testIfOffsetIs0(): void
     {
-        $request = $this->getMockBuilder(Request::class)
-            ->getMock();
-
-        $request->expects($this->at(0))
-            ->method('get')
-            ->with($this->equalTo('filters'), $this->equalTo(''))
-            ->willReturn('{"foo": "bar"}');
-
-        $request->expects($this->at(1))
-            ->method('get')
-            ->with($this->equalTo('sortBy'))
-            ->willReturn('foo');
-
-        $request->expects($this->at(2))
-            ->method('get')
-            ->with($this->equalTo('descending'))
-            ->willReturn(false);
-
-        $request->expects($this->at(3))
-            ->method('get')
-            ->with($this->equalTo('rowsPerPage'))
-            ->willReturn(10);
-
-        $request->expects($this->at(4))
-            ->method('get')
-            ->with($this->equalTo('page'))
-            ->willReturn(1);
-
-        $requestParams = RequestParams::fromRequest($request);
+        $this->request->get('filters', '{}')->willReturn('{"foo": "bar"}');
+        $this->request->get(Argument::cetera())->willReturn(null);
+        $this->request->get('rowsPerPage')->willReturn(10);
+        $this->request->get('page')->willReturn(1);
+        $requestParams = RequestParams::fromRequest($this->request->reveal());
         $requestParamsValue = $requestParams->get('offset');
         $this->assertEquals(0, $requestParamsValue);
     }
